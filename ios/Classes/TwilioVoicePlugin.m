acf26c0d38b2a2a5f37c031de1c4023e88f769d3
Mr. Schizo <mrschizo@macMini.local>
2022-03-18 12:16:14 +0100

    iOS Pod Settings

--- ios/Classes/TwilioVoicePlugin.m
+++ ios/Classes/TwilioVoicePlugin.m
@@ -1,11 +1,11 @@
 #import "TwilioVoicePlugin.h"
-#if __has_include(<twilio_voice/twilio_voice-Swift.h>)
-#import <twilio_voice/twilio_voice-Swift.h>
+#if __has_include(<twilio_voice_mimp/twilio_voice_mimp-Swift.h>)
+#import <twilio_voice_mimp/twilio_voice_mimp-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "twilio_voice-Swift.h"
+#import "twilio_voice_mimp-Swift.h"
 #endif
 
 @implementation TwilioVoicePlugin
